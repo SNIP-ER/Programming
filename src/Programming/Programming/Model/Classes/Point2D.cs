d8525706cd5b44f8f5afbf730e5687d824a28a3b
Lev <gorkaltsevgorkaltsev@gmail.com@gmail.com>
2026-03-09 15:46:36 +0700

    feature(Point2d.cs): Добавлен класс Point2D с readonly полями.

---
+++ src/Programming/Programming/Model/Classes/Point2D.cs
@@ -0,0 +1,17 @@
+﻿class Point2D
+{
+    public readonly float StoreX { get; private set; }
+    public readonly float StoreY { get; private set; }
+
+    // Конструктор
+    public Point2D(float storeX, float storeY)
+    {
+        StoreX = storeX;
+        StoreY = storeY;    
+    }
+
+    public Point2D()
+    {
+
+    }
+}
