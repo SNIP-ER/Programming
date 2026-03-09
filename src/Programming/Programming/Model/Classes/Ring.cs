96a736467e0c54c4c96f5364c4fd66bd16d858b9
Lev <155455286+SNIP-ER@users.noreply.github.com>
2026-03-09 20:30:26 +0700

    feature(Ring.cs): Добавллен класс Ring.

---
+++ src/Programming/Programming/Model/Classes/Ring.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Ring
+{
+    public Point2D Center { get; set; }
+    public double Area { get; private set; }
+
+    private float outerRadius, innerRadius;
+
+    public float OuterRadius
+    {
+        get { return outerRadius; }
+        set
+        {
+            if (outerRadius >= innerRadius)
+            {
+                outerRadius = Validator.AssertOnPositiveValue(value, "OuterRadius");
+            }
+            else
+            {
+                throw new ArgumentException(String.Format("Внешний радиус не может быть меньше внутреннего!"));
+            }
+        }
+    }
+
+    public float InnerRadius
+    {
+        get { return innerRadius; }
+        set
+        {
+            if (innerRadius <= outerRadius)
+            {
+                innerRadius = Validator.AssertOnPositiveValue(value, "InnerRadius");
+            }
+            else
+            {
+                throw new ArgumentException(String.Format("Внутренний радиус не может быть больше внешнего!"));
+            }
+        }
+    }
+
+    // Конструктор
+    public Ring(Point2D center, float outerRadius, float innerRadius)
+    {
+        Center = center;
+        OuterRadius = outerRadius;
+        InnerRadius = innerRadius;
+        Area = Math.PI * (Math.Pow(OuterRadius, 2) - Math.Pow(InnerRadius, 2));
+    }
+    public Ring()
+    {
+
+    }
+}
